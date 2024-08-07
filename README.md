483933ba0f814b4935b15eb7f6cf1edd77103cfd
louislhotte <louis.lhotte@student-cs.fr>
2024-08-07 15:15:31 +0200

    Manim Tutorial

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Manim Project: Depth-First Search Animation
-
+![Manim Animation Repository](Manim.png)
 ## Overview
 
 This project is dedicated to creating animations using Manim, a powerful mathematical animation engine. The main objective is to visually illustrate the Depth-First Search (DFS) algorithm through an engaging and educational video. The animations are created in Python, leveraging Manim's extensive capabilities to produce high-quality visuals.
 
 ## Video Link
+- Manim Tutorial - 10 Objects Display [ENG]: [Manim Tutorial](https://youtu.be/ZsVbCt0uT0M)
 - DFS animation [FR]: [DFS Video](https://youtu.be/prcsjvhN_c8?si=x5BY5rC3O7wk8ZSK) 
 - DFS animation [ENG]: [DFS Video](https://youtu.be/gcrqye-KYvI?si=fzHtF3jvvSbl9dQV) 
 - French Executives Salaries Evolution (1996 to 2022) [ENG]: [Salaries](https://youtu.be/rVqmQHxI0p4) 
 - Double pendulum animation - Butterfly effect | ManimCE [ENG]: [Double Pendulum](https://youtu.be/k4zENntIkM0) 
 - Statistic and parametric models [ENG]: [Handbook of statistics - Part I](https://youtu.be/gGh_hHVSbD8)
 - Gaussian Distribution Visualisation [ENG]: [Handbook of statistics - Part II](https://youtu.be/vMAus69cC74)
+- Taylor Series [ENG]: [Handbook of statistics - Part III](https://youtu.be/qcLvkmPo7xo)
 
 ### Files
 - 'XXXXX.py' : Animation python file used to generate videos and create media folder of wanted quality.
--- animations/objects.py
+++ animations/objects.py
@@ -1,21 +1,133 @@
 from manim import *
 
-def define_neural_network():
-    # Create the neural network (simplified representation)
-    layers = VGroup()
-    input_layer = VGroup(*[Dot() for _ in range(4)]).arrange(DOWN, buff=0.5)
-    hidden_layer = VGroup(*[Dot() for _ in range(10)]).arrange(DOWN, buff=0.5)
-    output_layer = VGroup(*[Dot() for _ in range(2)]).arrange(DOWN, buff=0.5)
+class TransformObjects(Scene):
+    def construct(self):
+        intro_group = self.introduction("Manim Tutorial (Part I)", 
+                                        "Learn How to Display 10 Objects in 60 seconds")
+        self.play(FadeOut(intro_group))
+        self.wait(1)
+        objects = [
+            Circle(),
+            Square(),
+            Triangle(),
+            RegularPolygon(5),  # Pentagon
+            RegularPolygon(6),  # Hexagon
+            Star(),
+            Ellipse(),
+            Rectangle(),
+            Line(start=ORIGIN, end=UP),
+            Arrow(start=ORIGIN, end=UP),
+        ]
+        
+        labels = [
+            "Circle",
+            "Square",
+            "Triangle",
+            "Pentagon",
+            "Hexagon",
+            "Star",
+            "Ellipse",
+            "Rectangle",
+            "Line",
+            "Arrow",
+        ]
+        
+        # Initialize first object and label
+        current_object = objects[0]
+        current_label = Text(labels[0]).next_to(current_object, DOWN)
+        
+        self.add(current_object, current_label)
+        self.wait(1)
 
-    layers.add(input_layer, hidden_layer, output_layer).arrange(RIGHT, buff=1.5)
+        # Loop through the remaining objects
+        for i in range(1, len(objects)):
+            new_object = objects[i]
+            new_label = Text(labels[i]).next_to(new_object, DOWN)
+            
+            self.play(Transform(current_object, new_object), Transform(current_label, new_label))
+            self.wait(3)
+        
+        self.wait(2)
+        self.play(FadeOut(current_object, current_label, new_label, new_object))
+        self.display_code(r'''
+class TransformObjects(Scene):
+    def construct(self):
+        objects = [
+            Circle(), Square(), Triangle(),
+            RegularPolygon(5), RegularPolygon(6),
+            Star(), Ellipse(), Rectangle(),
+            Line(start=ORIGIN, end=UP), Arrow(start=ORIGIN, end=UP),
+        ]
+        
+        labels = [
+            "Circle", "Square", "Triangle", "Pentagon", 
+            "Hexagon", "Star", "Ellipse", "Rectangle", "Line", "Arrow",
+        ]
+        
+        current_object = objects[0]
+        current_label = Text(labels[0]).next_to(current_object, DOWN)
+        
+        self.add(current_object, current_label)
+        self.wait(1)
 
-    edges = VGroup()
-    for start_layer, end_layer in zip(layers[:-1], layers[1:]):
-        for start_dot in start_layer:
-            for end_dot in end_layer:
-                edges.add(Line(start_dot.get_center(), end_dot.get_center()))
+        for i in range(1, len(objects)):
+            new_object = objects[i]
+            new_label = Text(labels[i]).next_to(new_object, DOWN)
+            self.play(Transform(current_object, new_object), Transform(current_label, new_label))
+            self.wait(3)
+        
+        self.wait(2))''')
 
-    neural_network = VGroup(layers, edges)
-    return neural_network
 
+    def display_code(self, code_text):
+        code = Code(
+            code=code_text,
+            tab_width=4,
+            font="Monospace",
+            background="window",  # "window" to keep syntax highlighting
+            insert_line_no=True,
+            language="Python"
+        ).scale(0.5)
+        code.background_mobject.set_fill(opacity=0) 
+
+        self.play(Write(code, run_time=15))  
+        self.wait(2)
+        self.play(FadeOut(code))
+
+
+    def define_neural_network():
+        # Create the neural network (simplified representation)
+        layers = VGroup()
+        input_layer = VGroup(*[Dot() for _ in range(4)]).arrange(DOWN, buff=0.5)
+        hidden_layer = VGroup(*[Dot() for _ in range(10)]).arrange(DOWN, buff=0.5)
+        output_layer = VGroup(*[Dot() for _ in range(2)]).arrange(DOWN, buff=0.5)
+
+        layers.add(input_layer, hidden_layer, output_layer).arrange(RIGHT, buff=1.5)
+
+        edges = VGroup()
+        for start_layer, end_layer in zip(layers[:-1], layers[1:]):
+            for start_dot in start_layer:
+                for end_dot in end_layer:
+                    edges.add(Line(start_dot.get_center(), end_dot.get_center()))
+
+        neural_network = VGroup(layers, edges)
+        return neural_network
+    
+    def introduction(self, title1, title2):
+        header = Tex(title1)
+        header.set_width(8)
+        from_pos = [header.get_left()[0] - 1, header.get_bottom()[1] - 0.5, 0]
+        to_pos = [header.get_right()[0] + 1, header.get_bottom()[1] - 0.5, 0]
+        line = Line(from_pos, to_pos)
+        writer = Tex("Created by Ptolémé")
+        writer_pos = [(line.get_left()[0] + line.get_right()[0]) / 2, line.get_bottom()[1] - 1, 0]
+        writer.move_to(writer_pos)
+        
+        self.play(Write(header), Write(line))
+        self.wait(0.5)
+        self.play(Transform(header, Tex(title2)))
+        self.play(Write(writer))
+        self.wait(8)
+        
+        return VGroup(header, writer, line)
 
